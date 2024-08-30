696829c565511749b5540b92e23911b9b7ed48fc
Dimple Patel <dimplepatel@webtechsystem.com>
2024-08-30 15:54:22 +0530

    .

--- blocks/products/products.js
+++ blocks/products/products.js
@@ -151,7 +151,7 @@ async function fetchData(block) {
                const button = document.createElement('button');
                button.textContent = 'View Details'; // Button text
                button.onclick = () => {
-                window.location.href = `https://main--edsdemo--abbacustechnology.hlx.live/product-detail?productDetail=${JSON.stringify(product)}`
+                window.location.href = `https://main--edsdemo--abbacustechnology.hlx.live/product-detail?title=${product.title}`
                };
                actionCell.appendChild(button)
                row.appendChild(actionCell);
