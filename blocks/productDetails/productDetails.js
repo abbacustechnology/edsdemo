f1cecb40c77501336b0765cdceea48cdec73e264
Dimple Patel <dimplepatel@webtechsystem.com>
2024-08-30 15:42:31 +0530

    .

---
+++ blocks/productDetails/productDetails.js
@@ -0,0 +1,11 @@
+export default async function decorate (block){
+   await fetchDataFromUrl(block)
+    console.log("functionData---->",JSON.stringify(block))
+}
+
+async function fetchDataFromUrl (block){
+    const urlParams = new URLSearchParams(window.location.search);
+    const data = urlParams.get('data');
+    
+    console.log('Data from query parameter:', data);
+}
